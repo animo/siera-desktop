eaece19950def59fbecd5e8273ed63419656cb34
Tom <tom@devv.nl>
2022-10-11 14:43:28 +0200

    feat: Initialisation with the Animo TypeScript template

--- webpack.plugins.js
+++
@@ -1,4 +0,0 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-
-module.exports = [new ForkTsCheckerWebpackPlugin(), new ReactRefreshWebpackPlugin()];
