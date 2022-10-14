892726372bfa36d849d378e48cd426f9da94b253
Tom Lanser <tom@animo.id>
2022-10-14 11:59:21 +0200

    fix: Processed almost all PR feedback
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-electron/src/App.tsx
+++ packages/toolbox-electron/src/App.tsx
@@ -1,27 +1,13 @@
 import { useAgent } from '@aries-framework/react-hooks'
 import React from 'react'
 
-const getCircularReplacer = () => {
-  const seen = new WeakSet()
-  return (key: any, value: any) => {
-    if (typeof value === 'object' && value !== null) {
-      if (seen.has(value)) {
-        return
-      }
-      seen.add(value)
-    }
-    return value
-  }
-}
-
 export function App() {
   const { agent } = useAgent()
 
   return (
     <>
       <div>
-        <h1>test</h1>
-        <p>Agent: {JSON.stringify(agent, getCircularReplacer())}</p>
+        <h1>Hellloooo world</h1>
       </div>
     </>
   )
