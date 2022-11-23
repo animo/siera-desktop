c064cf5a4efe09f72f697510ce0f909824a23206
Tom Lanser <tom@animo.id>
2022-11-23 13:02:12 +0100

    fix: Added a Content Security Policy for images inside the electron app
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-electron/forge.config.js
+++ packages/toolbox-electron/forge.config.js
@@ -32,7 +32,7 @@ module.exports = {
           liveReload: false,
           hot: true,
         },
-        devContentSecurityPolicy: `default-src 'self' 'unsafe-inline' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline' data:; connect-src * wss: ws:`,
+        devContentSecurityPolicy: `default-src 'self' 'unsafe-inline' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline' data:; connect-src * wss: ws:; img-src * https:`,
         mainConfig: './webpack.main.config.js',
         renderer: {
           config: './webpack.renderer.config.js',
