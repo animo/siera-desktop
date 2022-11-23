b36dc2f046dcdfc609f8defab743c17520c726d0
Tom Lanser <tom@animo.id>
2022-11-23 12:56:10 +0100

    fix: Added caching for the native dependency
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-electron/webpack.plugins.js
+++ packages/toolbox-electron/webpack.plugins.js
@@ -1,8 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const relocateLoader = require('@vercel/webpack-asset-relocator-loader')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
-/* eslint-enable @typescript-eslint/no-var-requires */
+/* eslint-enable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 
-module.exports = [new ForkTsCheckerWebpackPlugin(), new ReactRefreshWebpackPlugin()]
+module.exports = [
+  new ForkTsCheckerWebpackPlugin(),
+  new ReactRefreshWebpackPlugin(),
+
+  // https://www.electronforge.io/config/plugins/webpack#hot-reload-caching
+  {
+    apply(compiler) {
+      compiler.hooks.compilation.tap('webpack-asset-relocator-loader', (compilation) => {
+        relocateLoader.initAssetCache(compilation, 'native_modules')
+      })
+    },
+  },
+]
