33416479a56808217cc68f28db1814f2383158bf
Tom Lanser <tom@animo.id>
2022-12-09 16:10:11 +0100

    feat: Removed status from the proof loading array
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-core/src/utils/records/ProofsUtil.ts
+++ packages/toolbox-core/src/utils/records/ProofsUtil.ts
@@ -3,7 +3,7 @@ import type { ProofExchangeRecord } from '@aries-framework/core'
 import { ProofState } from '@aries-framework/core'
 
 export class ProofsUtil {
-  private static loadingStates: ProofState[] = [ProofState.PresentationSent, ProofState.RequestSent]
+  private static loadingStates: ProofState[] = [ProofState.RequestSent]
 
   private static acceptStates: ProofState[] = [ProofState.RequestReceived]
   private static declineStates: ProofState[] = [ProofState.RequestReceived]
