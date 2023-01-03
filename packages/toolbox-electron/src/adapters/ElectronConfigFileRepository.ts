0b155f2d9910572c6a58bc8c7b296ae8747f94b4
Tom Lanser <tom@animo.id>
2023-01-03 11:07:04 +0100

    feat: Added some validation for reading the config from disk
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-electron/src/adapters/ElectronConfigFileRepository.ts
+++ packages/toolbox-electron/src/adapters/ElectronConfigFileRepository.ts
@@ -1,6 +1,6 @@
 import type { ConfigFileRepository, ToolboxConfig } from '@animo/toolbox-core'
 
-import { DefaultConfiguration } from '@animo/toolbox-core'
+import { DefaultConfiguration, validateToolboxConfig } from '@animo/toolbox-core'
 
 export class ElectronConfigFileRepository implements ConfigFileRepository {
   public constructor(public readonly configPath: string) {}
@@ -11,7 +11,16 @@ export class ElectronConfigFileRepository implements ConfigFileRepository {
 
     // TODO: think about how to handle new config options
     const configString = await window.fs.read(this.configPath)
-    return JSON.parse(configString) as ToolboxConfig
+
+    const config = JSON.parse(configString) as ToolboxConfig
+
+    validateToolboxConfig(config)
+
+    return config
+  }
+
+  private validateToolboxConfig(config: ToolboxConfig) {
+    validateToolboxConfig(config)
   }
 
   public async writeConfiguration(config: ToolboxConfig): Promise<void> {
