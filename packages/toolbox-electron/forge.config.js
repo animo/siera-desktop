3b30c3770b5aaa894ee529b31769893a2f361c91
Tom Lanser <tom@animo.id>
2023-01-18 14:16:01 +0100

    feat: Added a patch to allow a --deep sign
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/toolbox-electron/forge.config.js
+++ packages/toolbox-electron/forge.config.js
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { executionAsyncId } = require('async_hooks')
+const { execSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+/* eslint-disable no-console */
+
 const nodeenv = process.env.NODE_ENV
 const isProd = nodeenv === 'production'
 
 module.exports = {
   packagerConfig: {
-    name: 'toolbox-electron',
-    executableName: 'toolbox-electron',
+    name: 'SieraDesktop',
+    executableName: 'siera-desktop',
     appBundleId: 'id.animo.siera.ui',
     osxSign: isProd && {
       identity: 'Developer ID Application: Animo Solutions (G9667JTP83)',
@@ -19,6 +30,21 @@ module.exports = {
       appleApiKey: process.env.SIERA_APPLE_API_KEY_PATH,
       appleApiKeyId: process.env.SIERA_APPLE_API_KEY_ID,
       appleApiIssuer: process.env.SIERA_APPLE_API_ISSUER,
+    },
+  },
+  hooks: {
+    packageAfterCopy: async (config, buildPath, electronVersion, platform, arch) => {
+      if (platform === 'darwin') {
+        const rootpath = `${buildPath}/../..`
+
+        console.log(execSync(`ls -la ${rootpath}`).toString())
+
+        const libindyPath = `${__dirname}/../../libs/libindy.universal.dylib`
+
+        console.log(execSync(`ls -la ${libindyPath}`).toString())
+
+        console.log(execSync(`cp ${libindyPath} ${rootpath}/MacOS/libindy.dylib`).toString())
+      }
     },
   },
   electronRebuildConfig: {},
