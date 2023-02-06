0e1ee02f18f999eb762098ae7363711e2b1c6bab
Tom Lanser <tom@animo.id>
2023-02-06 14:04:04 +0100

    First try to build a linux appimage
    Signed-off-by: Tom Lanser <tom@animo.id>

--- packages/siera-desktop/forge.config.js
+++ packages/siera-desktop/forge.config.js
@@ -70,6 +70,12 @@ module.exports = {
         },
       },
     },
+    {
+      name: 'electron-forge-maker-appimage',
+      config: {
+        bin: 'siera-desktop',
+      },
+    },
   ],
   plugins: [
     [
